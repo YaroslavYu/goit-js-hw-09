e502ace0dc571663bcaf7f1c155a439349c24f89
YaroslavYu <yaroslavyu801@gmail.com>
2022-12-18 21:03:02 +0200

    add 2 and 3 tasks

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,11 +2,19 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-Notify.failure('Qui timide rogat docet negare');
-Notify.success('Sol lucet omnibus');
-Notify.info('Cogito ergo sum');
+// const timerFields = document.querySelectorAll('.value');
 
-console.log('hhhh');
+const spanDaysLeft = document.querySelector('[data-days]');
+const spanHoursLeft = document.querySelector('[data-hours]');
+const spanMinutesLeft = document.querySelector('[data-minutes]');
+const spanSecondsLeft = document.querySelector('[data-seconds]');
+const buttonStart = document.querySelector('[data-start]');
+const timerWrapper = document.querySelector('.timer');
+
+let differenceMilliseconds = 0;
+let intervalId;
+buttonStart.disabled = true;
+buttonStart.addEventListener('click', onClickButtonStartTimerBegin);
 
 const options = {
   enableTime: true,
@@ -14,10 +22,96 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const chooseDate = selectedDates[0];
+
+    checkDataIsValid(chooseDate);
   },
 };
 
 // const dateTimePiker = document.querySelector('#datetime-picker');
 
-const rer = flatpickr('#datetime-picker', options);
+const dateTimePiker = flatpickr('#datetime-picker', options);
+
+timerWrapper.style.display = 'flex';
+timerWrapper.style.gap = '15px';
+
+// timerFields.forEach(field => (field.style.fontSize = '32px'));
+
+spanDaysLeft.style.fontSize = '32px';
+spanHoursLeft.style.fontSize = '32px';
+spanMinutesLeft.style.fontSize = '32px';
+spanSecondsLeft.style.fontSize = '32px';
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimer() {
+  const { days, hours, minutes, seconds } = convertMs(differenceMilliseconds);
+  spanDaysLeft.textContent = addLeadingZero(days);
+  spanHoursLeft.textContent = addLeadingZero(hours);
+  spanMinutesLeft.textContent = addLeadingZero(minutes);
+  spanSecondsLeft.textContent = addLeadingZero(seconds);
+  if (differenceMilliseconds < 1000) {
+    stopTimerIfNull();
+  }
+  differenceMilliseconds -= 1000;
+}
+
+function stopTimerIfNull() {
+  clearInterval(intervalId);
+}
+
+function checkDataIsValid(chooseDate) {
+  differenceMilliseconds = chooseDate - Date.now();
+  if (differenceMilliseconds < 0) {
+    Notify.failure('Please choose a date in the future');
+    return;
+  }
+  buttonStart.disabled = false;
+  const { days, hours, minutes, seconds } = convertMs(differenceMilliseconds);
+  spanDaysLeft.textContent = addLeadingZero(days);
+  spanHoursLeft.textContent = addLeadingZero(hours);
+  spanMinutesLeft.textContent = addLeadingZero(minutes);
+  spanSecondsLeft.textContent = addLeadingZero(seconds);
+  // timerFields.forEach(timerField =>
+  //   updateTimerFields(timerField, { days, hours, minutes, seconds })
+  // );
+}
+
+function onClickButtonStartTimerBegin() {
+  intervalId = setInterval(updateTimer, 1000);
+  buttonStart.disabled = true;
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+// function updateTimerFields(timerField, { days, hours, minutes, seconds }) {
+//   if (timerField.dataset.hasOenProperty('seconds')) {
+//     timerField.textContent = addLeadingZero(seconds);
+//   } else if (timerField.dataset.hasOenProperty('minutes')) {
+//     timerField.textContent = addLeadingZero(minutes);
+//   } else if (timerField.dataset.hasOenProperty('hours')) {
+//     timerField.textContent = addLeadingZero(hours);
+//   } else if (timerField.dataset.hasOenProperty('days')) {
+//     timerField.textContent = addLeadingZero(days);
+//   }
+//
+// }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,35 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmitCreatePromises);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
 }
 
-console.log('12121');
+function onFormSubmitCreatePromises(e) {
+  e.preventDefault();
+  let delay = Number(e.currentTarget.elements.delay.value);
+  const step = Number(e.currentTarget.elements.step.value);
+  const amount = Number(e.currentTarget.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+}
