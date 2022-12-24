c773eda5a2c0c8b663393019539d349ddb4158bb
YaroslavYu <yaroslavyu801@gmail.com>
2022-12-25 00:44:19 +0200

    refactoring timer with arrays

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,12 +2,12 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// const timerFields = document.querySelectorAll('.value');
+const timerFields = document.querySelectorAll('.value');
 
-const spanDaysLeft = document.querySelector('[data-days]');
-const spanHoursLeft = document.querySelector('[data-hours]');
-const spanMinutesLeft = document.querySelector('[data-minutes]');
-const spanSecondsLeft = document.querySelector('[data-seconds]');
+// const spanDaysLeft = document.querySelector('[data-days]');
+// const spanHoursLeft = document.querySelector('[data-hours]');
+// const spanMinutesLeft = document.querySelector('[data-minutes]');
+// const spanSecondsLeft = document.querySelector('[data-seconds]');
 const buttonStart = document.querySelector('[data-start]');
 const timerWrapper = document.querySelector('.timer');
 
@@ -35,12 +35,12 @@ const dateTimePiker = flatpickr('#datetime-picker', options);
 timerWrapper.style.display = 'flex';
 timerWrapper.style.gap = '15px';
 
-// timerFields.forEach(field => (field.style.fontSize = '32px'));
+timerFields.forEach(field => (field.style.fontSize = '32px'));
 
-spanDaysLeft.style.fontSize = '32px';
-spanHoursLeft.style.fontSize = '32px';
-spanMinutesLeft.style.fontSize = '32px';
-spanSecondsLeft.style.fontSize = '32px';
+// spanDaysLeft.style.fontSize = '32px';
+// spanHoursLeft.style.fontSize = '32px';
+// spanMinutesLeft.style.fontSize = '32px';
+// spanSecondsLeft.style.fontSize = '32px';
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -62,11 +62,14 @@ function convertMs(ms) {
 }
 
 function updateTimer() {
-  const { days, hours, minutes, seconds } = convertMs(differenceMilliseconds);
-  spanDaysLeft.textContent = addLeadingZero(days);
-  spanHoursLeft.textContent = addLeadingZero(hours);
-  spanMinutesLeft.textContent = addLeadingZero(minutes);
-  spanSecondsLeft.textContent = addLeadingZero(seconds);
+  // const { days, hours, minutes, seconds } = convertMs(differenceMilliseconds);
+  const time = convertMs(differenceMilliseconds);
+
+  // spanDaysLeft.textContent = addLeadingZero(days);
+  // spanHoursLeft.textContent = addLeadingZero(hours);
+  // spanMinutesLeft.textContent = addLeadingZero(minutes);
+  // spanSecondsLeft.textContent = addLeadingZero(seconds);
+  timerFields.forEach(timerField => updateTimerFields(timerField, time));
   if (differenceMilliseconds < 1000) {
     stopTimerIfNull();
   }
@@ -84,14 +87,14 @@ function checkDataIsValid(chooseDate) {
     return;
   }
   buttonStart.disabled = false;
-  const { days, hours, minutes, seconds } = convertMs(differenceMilliseconds);
-  spanDaysLeft.textContent = addLeadingZero(days);
-  spanHoursLeft.textContent = addLeadingZero(hours);
-  spanMinutesLeft.textContent = addLeadingZero(minutes);
-  spanSecondsLeft.textContent = addLeadingZero(seconds);
-  // timerFields.forEach(timerField =>
-  //   updateTimerFields(timerField, { days, hours, minutes, seconds })
-  // );
+  // const { days, hours, minutes, seconds } = convertMs(differenceMilliseconds);
+  const time = convertMs(differenceMilliseconds);
+
+  // spanDaysLeft.textContent = addLeadingZero(days);
+  // spanHoursLeft.textContent = addLeadingZero(hours);
+  // spanMinutesLeft.textContent = addLeadingZero(minutes);
+  // spanSecondsLeft.textContent = addLeadingZero(seconds);
+  timerFields.forEach(timerField => updateTimerFields(timerField, time));
 }
 
 function onClickButtonStartTimerBegin() {
@@ -103,15 +106,14 @@ function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 
-// function updateTimerFields(timerField, { days, hours, minutes, seconds }) {
-//   if (timerField.dataset.hasOenProperty('seconds')) {
-//     timerField.textContent = addLeadingZero(seconds);
-//   } else if (timerField.dataset.hasOenProperty('minutes')) {
-//     timerField.textContent = addLeadingZero(minutes);
-//   } else if (timerField.dataset.hasOenProperty('hours')) {
-//     timerField.textContent = addLeadingZero(hours);
-//   } else if (timerField.dataset.hasOenProperty('days')) {
-//     timerField.textContent = addLeadingZero(days);
-//   }
-//
-// }
+function updateTimerFields(timerField, { days, hours, minutes, seconds }) {
+  if (timerField.dataset.hasOwnProperty('seconds')) {
+    timerField.textContent = addLeadingZero(seconds);
+  } else if (timerField.dataset.hasOwnProperty('minutes')) {
+    timerField.textContent = addLeadingZero(minutes);
+  } else if (timerField.dataset.hasOwnProperty('hours')) {
+    timerField.textContent = addLeadingZero(hours);
+  } else if (timerField.dataset.hasOwnProperty('days')) {
+    timerField.textContent = addLeadingZero(days);
+  }
+}
